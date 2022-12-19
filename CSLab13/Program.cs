82882354af199414542af28bdb18544ffb024fb1
Dave <melza2000life@gmail.com>
2022-12-19 13:37:41 +0200

    setting up score table

--- CSLab13/Program.cs
+++ CSLab13/Program.cs
@@ -9,23 +9,22 @@ namespace CSLab13
             Console.Clear();
             List<string> ScoreBase = new List<string>();
             Branch br = new Branch();
-            while (true) { 
-                string tmp = (br.StartGame());    
-                ScoreBase.Add(tmp);
-                Console.WriteLine("\n" + tmp);        
-                Console.Write("" +
-                "\n![Продовжити? (1:так)]\n$ ");
-                string inp = Convert.ToString(Console.ReadLine()); 
-                if (inp != "1") { break; }
-            }
-            Console.Clear();
-
-            // Application.Init();
-            // Colors.Base.Normal = Application.Driver.MakeAttribute(Color.Green, Color.Black);
-            // Application.Run(new CSLab13.MyDialog());
-            // Application.Shutdown();
+            // while (true) { 
+            //     string tmp = (br.StartGame());    
+            //     ScoreBase.Add(tmp);
+            //     Console.WriteLine("\n" + tmp);        
+            //     Console.Write("" +
+            //     "\n![Продовжити? (1:так)]\n$ ");
+            //     string inp = Convert.ToString(Console.ReadLine()); 
+            //     if (inp != "1") { break; }
+            // }
+            // Console.Clear();
 
 
+            Application.Init();
+            Colors.Base.Normal = Application.Driver.MakeAttribute(Color.Green, Color.Black);
+            Application.Run(new CSLab13.MyDialog());
+            Application.Shutdown();
         }
     }
 }
